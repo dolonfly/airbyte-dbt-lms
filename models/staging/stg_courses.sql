206d8af70b122d1d71d21d8980a5cdec1a7d555b
dll <kaixin890312@163.com>
2023-12-07 21:21:46 +0800

    2 postgres

--- models/staging/stg_courses.sql
+++ models/staging/stg_courses.sql
@@ -4,11 +4,16 @@ with source AS (
 
 flattened_json_data AS (
   SELECT
-    JSON_EXTRACT(_airbyte_data,'$.id') AS id,
-    JSON_EXTRACT(_airbyte_data,'$.name') AS `name`,
-    JSON_EXTRACT(_airbyte_data,'$.workflow_state') AS workflow_state,
-    JSON_EXTRACT(_airbyte_data,'$.created_at') AS created_at,
-    JSON_EXTRACT(_airbyte_data,'$.updated_at') AS updated_at
+    -- JSON_EXTRACT(_airbyte_data,'$.id') AS id,
+    -- JSON_EXTRACT(_airbyte_data,'$.name') AS `name`,
+    -- JSON_EXTRACT(_airbyte_data,'$.workflow_state') AS workflow_state,
+    -- JSON_EXTRACT(_airbyte_data,'$.created_at') AS created_at,
+    -- JSON_EXTRACT(_airbyte_data,'$.updated_at') AS updated_at
+    _airbyte_data.JSONExtractValue('$.id') AS id,
+    _airbyte_data.JSONExtractValue('$.name') AS `name`,
+    _airbyte_data.JSONExtractValue('$.workflow_state') AS workflow_state,
+    _airbyte_data.JSONExtractValue('$.created_at') AS created_at,
+    _airbyte_data.JSONExtractValue('$.updated_at') AS updated_at
   FROM source
 )
 
--- models/staging/stg_users.sql
+++ models/staging/stg_users.sql
@@ -4,14 +4,22 @@ with source AS (
 
 flattened_json_data AS (
   SELECT
-    JSON_EXTRACT(_airbyte_data,'$.id') AS id,
-    JSON_EXTRACT(_airbyte_data,'$.name') AS user_name,
-    JSON_EXTRACT(_airbyte_data,'$.uuid') AS uuid,
-    JSON_EXTRACT(_airbyte_data,'$.phone') AS phone,
-    JSON_EXTRACT(_airbyte_data,'$.lti_id') AS lti_id,
-    JSON_EXTRACT(_airbyte_data,'$.created_at') AS created_at,
-    JSON_EXTRACT(_airbyte_data,'$.deleted_at') AS deleted_at,
-    JSON_EXTRACT(_airbyte_data,'$.updated_at') AS updated_at
+    -- JSON_EXTRACT(_airbyte_data,'$.id') AS id,
+    -- JSON_EXTRACT(_airbyte_data,'$.name') AS user_name,
+    -- JSON_EXTRACT(_airbyte_data,'$.uuid') AS uuid,
+    -- JSON_EXTRACT(_airbyte_data,'$.phone') AS phone,
+    -- JSON_EXTRACT(_airbyte_data,'$.lti_id') AS lti_id,
+    -- JSON_EXTRACT(_airbyte_data,'$.created_at') AS created_at,
+    -- JSON_EXTRACT(_airbyte_data,'$.deleted_at') AS deleted_at,
+    -- JSON_EXTRACT(_airbyte_data,'$.updated_at') AS updated_at
+    _airbyte_data.JSONExtractValue('$.id') AS id,
+    _airbyte_data.JSONExtractValue('$.name') AS user_name,
+    _airbyte_data.JSONExtractValue('$.uuid') AS uuid,
+    _airbyte_data.JSONExtractValue('$.phone') AS phone,
+    _airbyte_data.JSONExtractValue('$.lti_id') AS lti_id,
+    _airbyte_data.JSONExtractValue('$.created_at') AS created_at,
+    _airbyte_data.JSONExtractValue('$.deleted_at') AS deleted_at,
+    _airbyte_data.JSONExtractValue('$.updated_at') AS updated_at
   FROM source
 )
 
