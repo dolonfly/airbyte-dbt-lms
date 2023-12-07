799186613be187f872f7189b638891bf0b9ad2e6
dll <kaixin890312@163.com>
2023-12-07 21:25:42 +0800

    f

--- models/staging/stg_courses.sql
+++ models/staging/stg_courses.sql
@@ -10,7 +10,7 @@ flattened_json_data AS (
     -- JSON_EXTRACT(_airbyte_data,'$.created_at') AS created_at,
     -- JSON_EXTRACT(_airbyte_data,'$.updated_at') AS updated_at
     _airbyte_data.JSONExtractValue('$.id') AS id,
-    _airbyte_data.JSONExtractValue('$.name') AS `name`,
+    _airbyte_data.JSONExtractValue('$.name') AS name,
     _airbyte_data.JSONExtractValue('$.workflow_state') AS workflow_state,
     _airbyte_data.JSONExtractValue('$.created_at') AS created_at,
     _airbyte_data.JSONExtractValue('$.updated_at') AS updated_at
